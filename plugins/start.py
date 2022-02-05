efc784b65f94ce75166aefc902373c5d5cf137e0
Tujan2008 <98809445+Tujan2008@users.noreply.github.com>
2022-02-05 10:10:34 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -7,7 +7,8 @@ async def start(client, message):
     joinButton = InlineKeyboardMarkup([
         [InlineKeyboardButton("Channel 😇", url="https://t.me/+IU1ta7Gg19VkYzE1")],
         [InlineKeyboardButton(
-            "Report Bugs 🙈", url="https://t.me/tujan3")]
+            "Report Bugs 🙈", url="https://t.me/tujan3")],
+         [InlineKeyboardButton("Our Group 😇", url="https://t.me/trtechguide")]
     ])
     welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
     await message.reply_text(welcomed, reply_markup=joinButton)
