43af8395af82fcf781428ce90f4f3e9a21630c00
Tharuk Renuja <90763454+TharukRenuja@users.noreply.github.com>
2022-02-05 18:29:18 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,4 +1,23 @@
+import os
+import requests
+import time
+import traceback
+import logging
+import pyrogram
 from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, InlineKeyboardMarkup
+
+#Database
+import config
+from handlers.broadcast import broadcast
+from handlers.check_user import handle_user_status
+from handlers.database import Database
+
+LOG_CHANNEL = config.LOG_CHANNEL
+AUTH_USERS = config.AUTH_USERS
+DB_URL = config.DB_URL
+DB_NAME = config.DB_NAME
+
+db = Database(DB_URL, DB_NAME)
 
 
 @Client.on_message(Filters.command(["start"]), group=-2)
@@ -13,3 +32,25 @@ async def start(client, message):
     welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
+
+@Client.on_message(filters.private & filters.command("broadcast"))
+async def broadcast_handler_open(_, m):
+    if m.from_user.id not in AUTH_USERS:
+        await m.delete()
+        return
+    if m.reply_to_message is None:
+        await m.delete()
+    else:
+        await broadcast(m, db)
+
+
+@Client.on_message(filters.private & filters.command("stats"))
+async def sts(c, m):
+    if m.from_user.id not in AUTH_USERS:
+        await m.delete()
+        return
+    await m.reply_text(
+        text=f"**Total Users in Database 📂:** `{await db.total_users_count()}`",
+        parse_mode="Markdown",
+        quote=True
+    )    
